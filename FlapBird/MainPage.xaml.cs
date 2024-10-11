556cdca90689f6f56da3935f49a6a6b524b71c12
rafaela <reiisrafaela@icloud.com>
2024-10-11 10:56:44 -0300

    11/10

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -4,7 +4,10 @@ public partial class MainPage : ContentPage
 {
 	const int gravidade=30;
 	const int tempoEntreFrames=25;
-	bool estaMorto=false;
+	bool estaMorto=true;
+	double larguraJanela=0;
+	double alturaJanela=0;
+	int velocidade=20;
 	
 	public MainPage()
 	{
@@ -20,16 +23,43 @@ public partial class MainPage : ContentPage
 		{
 			AplicaGravidade();
 			await Task.Delay(tempoEntreFrames);
+			GerenciaCanos();
+
 		}
 
 	}
-    protected override void OnAppearing()
+    
+
+    protected override void OnSizeAllocated(double w, double h)
     {
-        base.OnAppearing();
-		Desenhar();
+        base.OnSizeAllocated(w, h);
+		larguraJanela=w;
+		alturaJanela=h;
     }
 
+	void GerenciaCanos()
+	{
+		CanoDeCima.TranslationX-=velocidade;	
+		CanoDeBaixo.TranslationX-=velocidade;
+		if(CanoDeBaixo.TranslationX<-larguraJanela)
+		{
+			CanoDeBaixo.TranslationX=0;
+			CanoDeCima.TranslationX=0;
+		}
+	
+	}
 
+	void OnGameOverClicked(object s, TappedEventArgs a)
+	{
+		FrameGameOver.IsVisible=false;
+		Inicializar();
+		Desenhar();
+	}
+	void Inicializar()
+	{
+		estaMorto=false;
+		passaro.TranslationY=0;
+	}
 
 }
 
