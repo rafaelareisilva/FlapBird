4bb09904359c0fdaefa50cfd00f265885f960bc2
rafaela <reiisrafaela@icloud.com>
2024-10-22 11:45:18 -0300

    22/10

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -2,20 +2,20 @@
 
 public partial class MainPage : ContentPage
 {
-	const int gravidade=10;
-	const int tempoEntreFrames=25;
-	bool estaMorto=true;
-	double larguraJanela=0;
-	double alturaJanela=0;
-	int velocidade=20;
-	const int maxTempoPulando=3;
-	int tempoPulando=0;
-	bool estaPulando=false;
-	const int forcaPulo=25;
+	const int gravidade = 10;
+	const int tempoEntreFrames = 25;
+	bool estaMorto = true;
+	double larguraJanela = 0;
+	double alturaJanela = 0;
+	int velocidade = 20;
+	const int maxTempoPulando = 3;
+	int tempoPulando = 0;
+	bool estaPulando = false;
+	const int forcaPulo = 25;
 	const int aberturaMinima = 200;
-	int score=0;
+	int score = 0;
 
-	
+
 	public MainPage()
 	{
 		InitializeComponent();
@@ -23,29 +23,29 @@ public partial class MainPage : ContentPage
 
 	void AplicaPulo()
 	{
-      passaro.TranslationY-=forcaPulo;
-	  tempoPulando++;
-	  if(tempoPulando>=maxTempoPulando)
-	  {
-		estaPulando=false;
-		tempoPulando=0;
-	  }
+		passaro.TranslationY -= forcaPulo;
+		tempoPulando++;
+		if (tempoPulando >= maxTempoPulando)
+		{
+			estaPulando = false;
+			tempoPulando = 0;
+		}
 	}
 
 	void AplicaGravidade()
 	{
-		passaro.TranslationY+=gravidade;
+		passaro.TranslationY += gravidade;
 	}
 	async Task Desenhar()
 	{
 		while (!estaMorto)
-		{ 
-		if (estaPulando)
-		     AplicaPulo();
+		{
+			if (estaPulando)
+				AplicaPulo();
 
-		else
-			AplicaGravidade();
-			
+			else
+				AplicaGravidade();
+
 			GerenciaCanos();
 
 			if (VerificaColisao())
@@ -57,26 +57,26 @@ public partial class MainPage : ContentPage
 			await Task.Delay(tempoEntreFrames);
 		}
 
-		}
+	}
 
-	
-    
 
-    protected override void OnSizeAllocated(double w, double h)
-    {
-        base.OnSizeAllocated(w, h);
-		larguraJanela=w;
-		alturaJanela=h;
-    }
+
+
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		larguraJanela = w;
+		alturaJanela = h;
+	}
 
 	void GerenciaCanos()
 	{
-		CanoDeCima.TranslationX-=velocidade;	
-		CanoDeBaixo.TranslationX-=velocidade;
-		if(CanoDeBaixo.TranslationX<=-larguraJanela)
+		CanoDeCima.TranslationX -= velocidade;
+		CanoDeBaixo.TranslationX -= velocidade;
+		if (CanoDeBaixo.TranslationX <= -larguraJanela)
 		{
-			CanoDeBaixo.TranslationX=4;
-			CanoDeCima.TranslationX=4;
+			CanoDeBaixo.TranslationX = 4;
+			CanoDeCima.TranslationX = 4;
 
 			var alturaMax = -100;
 			var alturaMin = -CanoDeBaixo.HeightRequest;
@@ -86,34 +86,46 @@ public partial class MainPage : ContentPage
 			score++;
 			labelScore.Text = "Canos:" + score.ToString("D3");
 		}
-	
+
 	}
 
 	void OnGameOverClicked(object s, TappedEventArgs a)
 	{
-		
+
 		Inicializar();
-		FrameGameOver.IsVisible=false;
+		FrameGameOver.IsVisible = false;
 		Desenhar();
 	}
+
 	void Inicializar()
 	{
-		estaMorto=false;
-		passaro.TranslationY=0;
+		estaMorto = false;
+
+		CanoDeCima.TranslationX = -larguraJanela;
+		CanoDeBaixo.TranslationX = -larguraJanela;
+		passaro.TranslationX = 0;
+		passaro.TranslationY = 0;
+		score = 0;
+		GerenciaCanos();
 	}
+
+
 	bool VerificaColisao()
 	{
 		if (!estaMorto)
 		{
 			if (VerificaColisaoTeto() ||
-			VerificaColisaoChao())
-			{
+			VerificaColisaoChao() ||
+			VerificaColisaoCanoCima())
+
+
 				return true;
-			}
 
 		}
+
 		return false;
 	}
+
 	bool VerificaColisaoTeto()
 	{
 		var minY = -alturaJanela / 2;
@@ -124,7 +136,7 @@ public partial class MainPage : ContentPage
 	}
 	bool VerificaColisaoChao()
 	{
-		var maxY = alturaJanela / 2 ;
+		var maxY = alturaJanela / 2;
 		if (passaro.TranslationY >= maxY)
 			return true;
 		else
@@ -134,8 +146,26 @@ public partial class MainPage : ContentPage
 
 	void OnGridClicked(object s, TappedEventArgs a)
 	{
-		estaPulando=true;
+		estaPulando = true;
 	}
+
+	bool VerificaColisaoCanoCima()
+	{
+		var posHpassaro = (larguraJanela / 2) - (passaro.WidthRequest / 2);
+		var posVpassaro = (alturaJanela / 2) - (passaro.HeightRequest / 2) + passaro.TranslationY;
+		if (posHpassaro >= Math.Abs(CanoDeCima.TranslationX) - CanoDeCima.WidthRequest &&
+		 posHpassaro <= Math.Abs(CanoDeCima.TranslationX) + CanoDeCima.WidthRequest &&
+		 posVpassaro <= CanoDeCima.HeightRequest + CanoDeCima.TranslationY)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
 
 }
 
